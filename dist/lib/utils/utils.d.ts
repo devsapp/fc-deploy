e52ce40dcaf2e44f0ab14433272aa7fcc4beafbf
千风 <qianfeng.zqf@alibaba-inc.com>
2021-07-08 14:39:42 +0800

    fix: fix deploy with build path problem
    deploy without build would use build path if path .s/build/artifacts exists

--- dist/lib/utils/utils.d.ts
+++ dist/lib/utils/utils.d.ts
@@ -1,4 +1,3 @@
-import ProgressBar from 'progress';
-export declare function createProgressBar(format: any, options: any): ProgressBar;
+export declare function createProgressBar(format: any, options: any): any;
 export declare function hasHttpPrefix(s: string): boolean;
 export declare function capitalizeFirstLetter(s: string): string;
