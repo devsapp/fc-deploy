4de4852dc290815763b710516eac4330cae780c5
千风 <qianfeng.zqf@alibaba-inc.com>
2021-07-29 17:53:43 +0800

    feat: upgrade version from 0.0.39 to 0.0.40

--- dist/lib/ignore.d.ts
+++ dist/lib/ignore.d.ts
@@ -1,2 +1,2 @@
-export declare function isIgnored(baseDir: string, runtime: string, ignoreRelativePath?: string): Promise<Function>;
-export declare function updateIgnore(baseDir: any, patterns: any): Promise<void>;
+export declare function isIgnoredInCodeUri(actualCodeUri: string, runtime: string): Promise<Function>;
+export declare function isIgnored(baseDir: string, runtime: string, actualCodeUri: string, ignoreRelativePath?: string): Promise<Function>;
