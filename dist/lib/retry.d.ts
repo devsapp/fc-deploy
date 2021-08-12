b63fe755ecfa737d5699a35562ced62ea3d04afe
千风 <qianfeng.zqf@alibaba-inc.com>
2021-08-12 16:42:08 +0800

    build: update build process
    move build to github action;add .signore;add -e option to ncc to make @serverless-devs/core out of
    ncc compile

--- dist/lib/retry.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function promiseRetry(fn: any): Promise<any>;
-export declare function retryDeployUntilSlsCreated(componentInstance: any, componentInputs: any): Promise<void>;
--- dist/lib/utils/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function createProgressBar(format: any, options: any): any;
-export declare function hasHttpPrefix(s: string): boolean;
-export declare function capitalizeFirstLetter(s: string): string;
