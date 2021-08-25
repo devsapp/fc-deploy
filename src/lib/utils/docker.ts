25d66ceb4c2f228c4253d7a775a8d27a9d8ee253
wss-git <wssgryx@163.com>
2021-08-25 15:57:42 +0800

    generate role update

--- src/lib/utils/docker.ts
+++ src/lib/utils/docker.ts
@@ -1,9 +1,9 @@
 import Docker from 'dockerode';
+
 const docker: any = new Docker();
 
 export async function imageExist(imageName: string): Promise<boolean> {
-
-  const images: Array<any> = await docker.listImages({
+  const images: any[] = await docker.listImages({
     filters: {
       reference: [imageName],
     },
