ce12c0a233e9f09a7b7d37679a9f98f0b727597d
wss-git <wssgryx@163.com>
2021-11-08 16:44:34 +0800

    fix: deploy sls auto retry 3

--- src/lib/retry.ts
+++ src/lib/retry.ts
@@ -19,15 +19,28 @@ export async function promiseRetry(fn: any): Promise<any> {
 
 export async function retryDeployUntilSlsCreated(componentInstance: any, componentInputs: any, retryTimes = 40) {
   let slsRetry = 0;
+  let retryNoPermission = slsRetry;
   do {
     try {
       await componentInstance.deploy(componentInputs);
       return;
     } catch (e) {
+      // 如果是报错日志不存在，则默认重试 40 次
+      // 如果日志报没有权限，则再 retry 3次，如果失败就抛出异常
+      // 否则就抛出异常
       if (isSlsNotExistException(e)) {
         slsRetry++;
+        retryNoPermission = slsRetry;
 
         if (slsRetry >= retryTimes) {
+          throw e;
+        }
+
+        logger.info(`Retrying service: It takes some effective time to create a log for the first time, retry ${slsRetry} time`);
+        await sleep(3000);
+      } else if (e?.message.includes('No permission to access the logstore')) {
+        slsRetry++;
+        if (slsRetry >= (retryNoPermission + 3)) {
           throw e;
         }
 
